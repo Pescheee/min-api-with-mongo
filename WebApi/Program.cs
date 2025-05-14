73d9aaa254e45fae90b4198062d1b2a48283f772
John Doe <johndoe@example.com>
2025-05-14 15:41:39 +0200

    edited files

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,38 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var movieDatabaseConfigSection = builder.Configuration.GetSection("DatabaseSettings");
+builder.Services.Configure<DatabaseSettings>(movieDatabaseConfigSection);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/check", (Microsoft.Extensions.Options.IOptions<DatabaseSettings> options) =>
+{
+    var mongoDbConnectionString = options.Value.ConnectionString;
+ 
+    var error = "Fehler beim Zugriff auf MongoDB";
+    try
+    {
+        var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        var cancellationToken = cancellationTokenSource.Token;
+ 
+        var client = new MongoClient(mongoDbConnectionString);
+ 
+        var databases = client.ListDatabaseNames(cancellationToken).ToList();
+ 
+        return Results.Ok("Zugriff auf MongoDB ok. Datenbanken: " + string.Join(", ", databases));
+    }
+    catch (TimeoutException ex)
+    {
+        return Results.Problem(error + " (timout): " + ex.Message);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(error + " :" + ex.Message);
+    }
+});
 
 app.Run();
